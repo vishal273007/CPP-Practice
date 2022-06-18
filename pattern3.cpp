a1f96e9a07e99534baa4a8a1d35d0393e2d233f5
Vishal Vishwakarma <vv273007@gmail.com>
2022-06-18 14:23:23 +0530

    added .gitignore

---
+++ pattern3.cpp
@@ -0,0 +1,30 @@
+// C++ program to print inverted pyramid.
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"Enter a number : ";
+    cin>>n;
+
+    // for(int i=n; i>=1; i--){
+    //     for(int j=1; j<=i; j++){
+    //         cout<<"*";
+    //     }
+    //     cout<<endl;
+    // }
+
+    for(int i=1; i<=n; i++){
+        for(int j=1;j<=n;j++){
+            if(j<=n-i){
+                cout<<" ";
+            }
+            else{
+                cout<<"* ";
+            }
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
